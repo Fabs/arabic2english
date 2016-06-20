29e306b107d80c4e6a650b4a5d51efbe7cadd962
Fabricio Nascimento <fabriciosn@gmail.com>
2016-06-19 22:38:55 -0300

    Adds support to 0: zero

--- lib/number_reader.rb
+++ lib/number_reader.rb
@@ -1,4 +1,5 @@
 module NumberReader
   def convert_arabic_number_to_english(number)
+    'zero'
   end
 end
--- spec/number_reader_spec.rb
+++ spec/number_reader_spec.rb
@@ -1,5 +1,10 @@
 require_relative '../lib/number_reader.rb'
 
 RSpec.describe NumberReader do
-  it { fail }
+  include NumberReader
+  alias_method 'convert', 'convert_arabic_number_to_english'
+
+  context '[0-9] decimals' do
+    it { expect(convert(0)).to eq('zero') }
+  end
 end
