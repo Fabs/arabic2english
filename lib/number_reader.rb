047f62c0be16f04a0eb1853069e4c7b960005639
Fabricio Nascimento <fabriciosn@gmail.com>
2016-06-19 23:04:29 -0300

    Add support tospecial numbers on 20-99 range

--- lib/number_reader.rb
+++ lib/number_reader.rb
@@ -1,10 +1,32 @@
 module NumberReader
+
+
+  def convert_arabic_number_to_english(number)
+    solver = unique_number_solver
+
+    solver[number]
+  end
+
+private
   DECIMALS = %w(zero one two three four five six seven eight nine)
   TWO_DIGIT = %w(ten eleven twelve thirteen fourteen fifteen sixteen seventeen
                  eighteen nineteen)
-  UNIQUES = DECIMALS + TWO_DIGIT
+  UNIQUE_NUMBERS = {
+    20 => 'twenty',
+    30 => 'thirty',
+    40 => 'fourty',
+    50 => 'fifty',
+    60 => 'sixty',
+    70 => 'seventy',
+    70 => 'seventy',
+    80 => 'eighty',
+    90 => 'ninety',
+  }
 
-  def convert_arabic_number_to_english(number)
-    UNIQUES[number]
+  def unique_number_solver
+    bellow_twenty = (DECIMALS + TWO_DIGIT).map.with_index{|number, i| [i, number]}
+    hash_bellow_twenty = Hash[ bellow_twenty ]
+
+    UNIQUE_NUMBERS.merge(hash_bellow_twenty)
   end
 end
--- spec/number_reader_spec.rb
+++ spec/number_reader_spec.rb
@@ -29,4 +29,21 @@ RSpec.describe NumberReader do
     it { expect(convert(18)).to eq('eighteen') }
     it { expect(convert(19)).to eq('nineteen') }
   end
+
+  context '[20-99] two-digit' do
+    context 'NOT following rule' do
+      it { expect(convert(20)).to eq('twenty') }
+      it { expect(convert(30)).to eq('thirty') }
+      it { expect(convert(40)).to eq('fourty') }
+      it { expect(convert(50)).to eq('fifty') }
+      it { expect(convert(60)).to eq('sixty') }
+      it { expect(convert(70)).to eq('seventy') }
+      it { expect(convert(80)).to eq('eighty') }
+      it { expect(convert(90)).to eq('ninety') }
+    end
+
+    context 'following simple rule' do
+
+    end
+  end
 end
