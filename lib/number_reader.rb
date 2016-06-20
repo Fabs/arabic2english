8ac694eb9d6b84e6e7834c43c0befc038aab6129
Fabricio Nascimento <fabriciosn@gmail.com>
2016-06-19 22:48:59 -0300

    Add support for numbers 10-19

--- lib/number_reader.rb
+++ lib/number_reader.rb
@@ -1,6 +1,10 @@
 module NumberReader
   DECIMALS = %w(zero one two three four five six seven eight nine)
+  TWO_DIGIT = %w(ten eleven twelve thirteen fourteen fifteen sixteen seventeen
+                 eighteen nineteen)
+  UNIQUES = DECIMALS + TWO_DIGIT
+
   def convert_arabic_number_to_english(number)
-    DECIMALS[number]
+    UNIQUES[number]
   end
 end
--- spec/number_reader_spec.rb
+++ spec/number_reader_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe NumberReader do
 
   context '[0-9] decimals' do
     it { expect(convert(0)).to eq('zero') }
-
     it { expect(convert(1)).to eq('one') }
     it { expect(convert(2)).to eq('two') }
     it { expect(convert(3)).to eq('three') }
@@ -16,5 +15,18 @@ RSpec.describe NumberReader do
     it { expect(convert(7)).to eq('seven') }
     it { expect(convert(8)).to eq('eight') }
     it { expect(convert(9)).to eq('nine') }
+  end
+
+  context '[10-19] two-digit numbers up to 19' do
+    it { expect(convert(10)).to eq('ten') }
+    it { expect(convert(11)).to eq('eleven') }
+    it { expect(convert(12)).to eq('twelve') }
+    it { expect(convert(13)).to eq('thirteen') }
+    it { expect(convert(14)).to eq('fourteen') }
+    it { expect(convert(15)).to eq('fifteen') }
+    it { expect(convert(16)).to eq('sixteen') }
+    it { expect(convert(17)).to eq('seventeen') }
+    it { expect(convert(18)).to eq('eighteen') }
+    it { expect(convert(19)).to eq('nineteen') }
   end
 end
